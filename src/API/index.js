007c7855f1b57091b33ea800b7446402945c6cf2
Joan Gavelán <joangavelan@gmail.com>
2021-08-07 01:02:02 -0500

    assets implemented

--- src/API/index.js
+++ src/API/index.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-export const getPosts = (query, page = 0) => axios.get(`https://hn.algolia.com/api/v1/search_by_date?query=${query}&page=${page}`);
+export const getHits = (query, page = 0) => axios.get(`https://hn.algolia.com/api/v1/search_by_date?query=${query}&page=${page}&hitsPerPage=8`);
--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const App = () => {
+
+  // const [filter, setFilter] = useState('');
+  // const [page, setPage] = useState(0);
+  // const [section, setSection] = useState('');
+  // const [hits, setHits] = useState([]);
+  // const [favHits, setFavHits] = useState([]);
+
+  // useEffect(() => {
+  //   getHits(filter, page).then(resp => {
+  //     const { data } = resp;
+  //     console.log(data);
+  //   });
+  // }, []);
+
+
   return (
     <div className="App">
-      
+      <h1>Hola</h1>
     </div>
   )
 }
