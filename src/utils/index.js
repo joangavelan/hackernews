10e0a90ce2ca69bdf89b0ae735016a7c2c6ca94b
Joan Gavelán <joangavelan@gmail.com>
2021-08-08 16:49:25 -0500

    get time diff function created

--- src/utils/index.js
+++ src/utils/index.js
@@ -21,3 +21,53 @@ export function useLocalStorage(key, initialValue) {
   }
   return [storedValue, setValue]
 }
+
+//get time difference from a specific timezone
+export function getTimeDifference(timezone) {
+  const now = new Date();
+  const created_at = new Date(timezone);
+  
+  const msPerMinute = 60 * 1000;
+  const msPerHour = msPerMinute * 60;
+  const msPerDay = msPerHour * 24;
+  const msPerMonth = msPerDay * 30;
+  const msPerYear = msPerDay * 365;
+
+  const elapsed = now - created_at;
+
+  let timeDiff = 0;
+
+  if(elapsed <= 0) {
+    return 'just now';
+  }
+
+  else if (elapsed < msPerMinute) {
+    timeDiff = Math.round(elapsed/1000);   
+    return `${timeDiff} ${timeDiff === 1 ? 'second ago' : 'seconds ago'}`
+  }
+
+  else if (elapsed < msPerHour) {
+    timeDiff = Math.round(elapsed/msPerMinute);   
+    return `${timeDiff} ${timeDiff === 1 ? 'minute ago' : 'minutes ago'}` 
+  }
+
+  else if (elapsed < msPerDay ) {
+    timeDiff = Math.round(elapsed/msPerHour);   
+    return `${timeDiff} ${timeDiff === 1 ? 'hour ago' : 'hours ago'}`   
+  }
+
+  else if (elapsed < msPerMonth) {
+    timeDiff = Math.round(elapsed/msPerDay);   
+    return `${timeDiff} ${timeDiff === 1 ? 'day ago' : 'days ago'}`  
+  }
+
+  else if (elapsed < msPerYear) {
+    timeDiff = Math.round(elapsed/msPerMonth);   
+    return `${timeDiff} ${timeDiff === 1 ? 'month ago' : 'months ago'}`  
+  }
+
+  else {
+    timeDiff = Math.round(elapsed/msPerYear);   
+    return `${timeDiff} ${timeDiff === 1 ? 'year ago' : 'years ago'}`   
+  }
+}
