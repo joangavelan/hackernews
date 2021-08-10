014106d956ffd9935c187431e5d85d93f8bef11d
Joan Gavelán <joangavelan@gmail.com>
2021-08-10 00:46:54 -0500

    className

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Loader.scss'
 
-const Loader = () =>  <div class="Loader"><div></div><div></div><div></div><div></div></div>
+const Loader = () =>  <div className="Loader"><div></div><div></div><div></div><div></div></div>
 
 export default Loader
