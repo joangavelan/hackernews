136630af682a389de9883fcbad364a08d6461dac
Joan Gavelán <joangavelan@gmail.com>
2021-08-11 02:16:06 -0500

    previous and next functions now also depends on the response state in order to avoid rendering wrong page data

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import './Pagination.scss'
 
-const Pagination = ({ section, hits, page, setPage }) => {
+const Pagination = ({ section, page, setPage, response }) => {
 
   const pageNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  const nextPage = () => page < pageNumbers.length ? setPage(page => page + 1) : false;
+  const nextPage = () => response && page < pageNumbers.length ? setPage(page => page + 1) : false;
 
-  const previousPage = () => page > 1 ? setPage(page => page - 1) : false;
+  const previousPage = () => response && page > 1 ? setPage(page => page - 1) : false;
 
   return (
-    hits.length > 0 && section === 'all' &&
+    section === 'all' &&
     <div className="Pagination">
       <div className="Pagination__item" onClick={() => previousPage()}>
         <i className="Pagination__arrow left"></i>
       </div>
       <ul className="Pagination__page-numbers">
-        {pageNumbers.map((pageNumber, index) => (
-          <div key={index} className={`Pagination__item ${page === pageNumber ? 'active' : ''}`} onClick={() => setPage(pageNumber)}>
+        {pageNumbers.map(pageNumber => (
+          <div key={pageNumber} className={`Pagination__item ${page === pageNumber ? 'active' : ''}`} onClick={() => setPage(pageNumber)}>
             <li className="Pagination__page-number" >{pageNumber}</li>
           </div>
         ))}
