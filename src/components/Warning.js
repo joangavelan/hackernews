0b8cf59292d32e138b88f6d3ca15ad21cf569af4
Joan Gavelán <joangavelan@gmail.com>
2021-08-11 02:07:54 -0500

    warning component created

---
+++ src/components/Warning.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Warning.scss'
+
+const Warning = () => {
+  return (
+    <div className="Warning">
+      <p className="Warning__message">We're sorry! There were no results for this page</p>
+    </div>
+  )
+}
+
+export default Warning
