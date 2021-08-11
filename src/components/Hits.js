43e50a2f21f33519a880d12d1f80f5631c4d8607
Joan Gavelán <joangavelan@gmail.com>
2021-08-11 02:10:44 -0500

    conditional rendering remove, it no longers depends on the component itself

--- src/components/Hits.js
+++ src/components/Hits.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import Hit from './Hit'
 import './Hits.scss'
-import Loader from './Loader'
 
 const Hits = ({ hits, faveSet }) => {
   return (
     <div className="Hits">
-      {hits.length > 0 
-      ? hits.map(hit => <Hit key={hit.id} hit={hit} faveSet={faveSet}/>)
-      : <Loader />}
+      {hits.map(hit => <Hit key={hit.id} hit={hit} faveSet={faveSet}/>)}
     </div>
   )
 }
